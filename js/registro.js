827d069a8bfb29b4dc79cb0ac872e9551d6ae331
El Bronco <2020-0288@uteco.edu.do>
2025-09-26 20:39:18 -0400

    Mechanics Industrial RG

---
+++ js/registro.js
@@ -0,0 +1,12 @@
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+const pass = document.getElementById("password")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    if(nombre.nodeValue.length<6){
+        alert("nombre muy conto")
+    }
+} )
